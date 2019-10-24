729a32d1ec01eed4942f3a025f5a032bb304662a
pgobrien <pgobrien@iu.edu>
2019-10-24 11:56:14 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mouse/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.mouse
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val leftButton = findViewById<Button>(R.id.leftMouseButton)
+        leftButton.setOnClickListener {
+            Toast.makeText(this, "leftButton", Toast.LENGTH_SHORT).show()
+        }
+
+        val rightButton = findViewById<Button>(R.id.rightMouseButton)
+        rightButton.setOnClickListener {
+            Toast.makeText(this, "rightButton", Toast.LENGTH_SHORT).show()
+        }
+
+        val testButton = findViewById<Button>(R.id.testButton)
+        rightButton.setOnClickListener {
+
+        }
+
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/mouse/TestScreen.kt
@@ -0,0 +1,12 @@
+package com.example.mouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_screen)
+    }
+}
